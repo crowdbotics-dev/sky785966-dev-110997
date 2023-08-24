d0378c6b90d4f08a9010a3feb213231f9b92fa15
Crowdbotics <engineering@crowdbotics.com>
2023-08-24 06:54:06 +0000

    Initial commit.

---
+++ backend/sky785966_dev_110997/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sky785966_dev_110997 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sky785966_dev_110997.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sky785966-dev-110997",
+  url: "https://sky785966-dev-110997.botics.co",
+  api: "https://sky785966-dev-110997.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
